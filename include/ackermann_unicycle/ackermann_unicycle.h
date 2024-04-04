2f289eb2a5eff3b837aa95354ad45e40897e4644
Pierpaolo <pierpaolo.serio@gmail.com>
2024-04-04 08:18:44 +0000

    Update

--- include/ackermann_unicycle/ackermann_unicycle.h
+++ include/ackermann_unicycle/ackermann_unicycle.h
@@ -8,6 +8,7 @@
 #include <Eigen/Core>
 #include <Eigen/Geometry>
 
+
 class AckermannUnicycle {
 public:
     AckermannUnicycle();
@@ -16,11 +17,13 @@ public:
     nav_msgs::Odometry getLatestOdometry(); // Assuming this method exists
 
     nav_msgs::Odometry odom_msg;
+    double theta_angle;
 
 private:
     ros::NodeHandle nh_;
     ros::Publisher odom_pub_;
 
+    double time;
     double x_, y_, z_, theta_; // robot pose
     double velocity_, angular_velocity_; // control inputs
 };
--- src/ackermann_unicycle.cpp
+++ src/ackermann_unicycle.cpp
@@ -1,29 +1,39 @@
-#include <Eigen/Geometry>
 #include <ackermann_unicycle/ackermann_unicycle.h>
 
 
 AckermannUnicycle::AckermannUnicycle() : nh_("~"), x_(0), y_(0), z_(0), theta_(0) {
     
-    this->velocity_ = 0.2;
-    this->angular_velocity_ = 0.1;
+    this->velocity_ = 0.3;
+    this->angular_velocity_ = 0;
+    this->time = 0;
 
     odom_pub_ = nh_.advertise<nav_msgs::Odometry>("odom_1", 50);
 }
 
 void AckermannUnicycle::publishOdometry() {
     double dt = 0.1; // time step
+    this->time += dt;
+    
+    
+
+    if (10 <= this->time && this->time <= 25) this->angular_velocity_= 0.2;
+    else if (25 <= this->time && this->time <= 35)  this->angular_velocity_ = -0.2;
+    else this->angular_velocity_ = 0.1;
+
+    angular_velocity_ = this->angular_velocity_;
+
 
     // Update the pose of the robot
     x_ += velocity_ * cos(theta_) * dt;
     y_ += velocity_ * sin(theta_) * dt;
     z_ += 0; // assuming motion only in the x-y plane
     theta_ += angular_velocity_ * dt;
-
+    this->theta_angle = theta_;
     // Publish odometry message
 
     this->odom_msg.header.stamp = ros::Time::now();
     this->odom_msg.header.frame_id = "map";
-    this->odom_msg.child_frame_id = "base_link";
+    this->odom_msg.child_frame_id = "map";
 
     // Position
     this->odom_msg.pose.pose.position.x = x_;
@@ -62,30 +72,42 @@ void TransformedOdometryPublisher::publishTransformedOdometry() {
     // Get the latest odometry message from AckermannUnicycle
     nav_msgs::Odometry odom_msg = this->ackermann_unicycle_.getLatestOdometry();
 
+    Eigen::Quaterniond pre_q;
+
+    pre_q.x() = 0.0;
+    pre_q.y() = 0.0;
+    pre_q.z() = sin(this->ackermann_unicycle_.theta_angle / 2.0);
+    pre_q.w() = cos(this->ackermann_unicycle_.theta_angle / 2.0);
+    
+    Eigen::Affine3d preRot;
+    preRot.linear() = pre_q.toRotationMatrix();
     // Apply transformation to the odometry message
     // For demonstration, let's just apply a translation of 1 unit in the x-axis
     
-    Eigen::Vector3d transl(0.1, 1.2, 0);
+    Eigen::Vector3d transl(0.1, -1.2, 0);
 
     Eigen::Quaterniond quat;
     quat.x() = 0;
     quat.y() = 0;
-    quat.z() = 0;
-    quat.w() = 1;
+    quat.z() = 0;//sin(3.14 / 2.0);
+    quat.w() = 1;//cos(3.14 / 2.0);
+
 
     Eigen::Affine3d transformation;
-    transformation.translation() = transl;
+    transformation.translation() = preRot.linear() * transl;
     transformation.linear() = quat.toRotationMatrix();
 
     // Convert geometry_msgs::Pose to Eigen::Affine3d
-    Eigen::Translation3d translation(odom_msg.pose.pose.position.x,
+    Eigen::Vector3d translation_fromMsg(odom_msg.pose.pose.position.x,
                                      odom_msg.pose.pose.position.y,
                                      odom_msg.pose.pose.position.z);
-    Eigen::Quaterniond orientation(odom_msg.pose.pose.orientation.w,
+    Eigen::Quaterniond orientation_fromMsg(odom_msg.pose.pose.orientation.w,
                                    odom_msg.pose.pose.orientation.x,
                                    odom_msg.pose.pose.orientation.y,
                                    odom_msg.pose.pose.orientation.z);
-    Eigen::Affine3d pose_affine = translation * orientation;
+    Eigen::Affine3d pose_affine;
+    pose_affine.translation() = translation_fromMsg;
+    pose_affine.linear() = orientation_fromMsg.toRotationMatrix();
 
     // Apply transformation
     Eigen::Affine3d transformed_pose_affine = transformation * pose_affine;
